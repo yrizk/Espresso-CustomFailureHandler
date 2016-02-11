3b25428a66ef003dbebf6f9a1cb824ac5dd6454b
yrizk <yohan.rizk@gmail.com>
2016-02-10 21:58:03 -0500

    now just holding onto a weak reference of the activity

--- app/src/androidTest/java/com/yohan/espressotest/MyFailureHandler.java
+++ app/src/androidTest/java/com/yohan/espressotest/MyFailureHandler.java
@@ -5,7 +5,6 @@ import android.net.Uri;
 import android.support.test.espresso.EspressoException;
 import android.support.test.espresso.FailureHandler;
 import android.support.test.espresso.PerformException;
-import android.support.test.espresso.core.deps.guava.base.Preconditions;
 import android.support.test.espresso.core.deps.guava.base.Throwables;
 import android.view.View;
 
@@ -13,17 +12,18 @@ import junit.framework.AssertionFailedError;
 
 import org.hamcrest.Matcher;
 
+import java.lang.ref.WeakReference;
+
 /**
  * Created by rizk on 2/3/16.
  */
 public class MyFailureHandler implements FailureHandler {
 
-    private static final String TAG = MyFailureHandler.class.getSimpleName();
-    private final Activity activityRef;
+    private final WeakReference<Activity> activityRef;
     private ScreenShooter screenShotManager;
 
     public MyFailureHandler(Activity ref) {
-        activityRef = Preconditions.checkNotNull(ref);
+        activityRef = new WeakReference<Activity>(ref);
         screenShotManager = new ScreenShooter(ref);
     }
 
@@ -31,9 +31,11 @@ public class MyFailureHandler implements FailureHandler {
     public void handle(Throwable error, Matcher<View> viewMatcher) {
         if (error instanceof EspressoException || error instanceof AssertionFailedError
                 || error instanceof AssertionError) {
-            Uri location = screenShotManager.shoot(activityRef.getWindow().getDecorView());
+            Uri location = Uri.EMPTY;
+            if (activityRef.get() != null) {
+                location = screenShotManager.shoot(activityRef.get().getWindow().getDecorView());
+            }
             throw Throwables.propagate(getUserFriendlyError(error, viewMatcher, location.toString()));
-
         } else {
             throw Throwables.propagate(error);
         }
@@ -46,7 +48,10 @@ public class MyFailureHandler implements FailureHandler {
      * directly where the actual problem is)
      */
     private Throwable getUserFriendlyError(Throwable error, Matcher<View> viewMatcher, String screenShotLocation) {
-        String formattedScreenLocation = " \n. Location of Screenshot:  \n " + screenShotLocation;
+        String formattedScreenLocation = "";
+        if (screenShotLocation != null) {
+            formattedScreenLocation = " \n. Location of Screenshot:  \n " + screenShotLocation;
+        }
         if (error instanceof PerformException) {
             // Re-throw the exception with the viewMatcher (used to locate the view) as the view
             // description (makes the error more readable). The reason we do this here: not all creators
